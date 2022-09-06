c05aaf01f60342e306f2e768d73b9c9e29d8c5c0
AgustinAguero <agustin.aguero789@gmail.com>
2022-09-06 15:54:52 -0300

    Busqueda y resultados de busqueda - Carga de imagenes

--- AppTienda/views.py
+++ AppTienda/views.py
@@ -36,7 +36,7 @@ def clientes(request):
             cliente=Cliente(nombre=nombre, apellido=apellido,telefono=telefono,mail=mail)
 #Guardamos en la base de datos con save y devolvemos a la pagina inicio cuando todo este correctamente cargado.            
             cliente.save()
-            return render (request, "AppTienda/inicio.html")
+            return render (request, "AppTienda/inicio.html", {"mensaje" : "Cliente Cargado"})
 
 #Si no se cargo correctamente, vuelve a mostrar el formulario a cargar vacio 
     else:
@@ -55,7 +55,7 @@ def zapatillas(request):
             precio=informacion["precio"]      
             zapatilla=Zapatilla(modelo=modelo, marca=marca,talle=talle,precio=precio)           
             zapatilla.save()
-            return render (request, "AppTienda/inicio.html")
+            return render (request, "AppTienda/inicio.html", {"mensaje" : "Zapatilla Cargada"})
     else:
         formulario=ArticuloForm()
         return render(request, "AppTienda/zapatillas.html", {"formulario": formulario})
@@ -71,7 +71,7 @@ def remeras(request):
             precio=informacion["precio"]      
             remera=Remera(modelo=modelo, marca=marca,talle=talle,precio=precio)           
             remera.save()
-            return render (request, "AppTienda/inicio.html")
+            return render (request, "AppTienda/inicio.html", {"mensaje" : "Remera Cargada"})
     else:
         formulario=ArticuloForm()
         return render(request, "AppTienda/remeras.html", {"formulario": formulario})   
@@ -87,7 +87,7 @@ def pantalones(request):
             precio=informacion["precio"]      
             pantalon=Pantalon(modelo=modelo, marca=marca,talle=talle,precio=precio)           
             pantalon.save()
-            return render (request, "AppTienda/inicio.html")
+            return render (request, "AppTienda/inicio.html", {"mensaje" : "Pantalon Cargado"})
     else:
         formulario=ArticuloForm()
         return render(request, "AppTienda/pantalones.html", {"formulario": formulario})
@@ -96,13 +96,48 @@ def pantalones(request):
 def busquedaClientes(request):
     return render(request,"AppTienda/busquedaClientes.html")
 
+def busquedaZapatillas(request):
+    return render(request,"AppTienda/busquedaZapatillas.html")
 
-def buscar(request):
+def busquedaRemeras(request):
+    return render(request,"AppTienda/busquedaRemeras.html")
+
+def busquedaPantalones(request):
+    return render(request,"AppTienda/busquedaPantalones.html")
+
+
+def buscarCliente(request):
     if request.GET["apellido"]:
         apellido = request.GET["apellido"]
         clientes=Cliente.objects.filter(apellido=apellido) #Traer de la base todos los clientes que tengan este apellido
         
-        return render(request, "AppCoder/resultadosBusquedaClientes.html", {"clientes":clientes})
+        return render(request, "AppTienda/resultadosBusquedaClientes.html", {"clientes":clientes})
     else:
-        return render(request, "AppCoder/busquedaClientes.html", {"mensaje":"Ingrese nuevamente apellido"})
+        return render(request, "AppTienda/busquedaClientes.html", {"mensaje":"Ingrese nuevamente apellido"})
 
+def buscarZapatilla(request):
+    if request.GET["marca"]:
+        marca = request.GET["marca"]
+        zapatillas=Zapatilla.objects.filter(marca=marca)
+        
+        return render(request, "AppTienda/resultadosBusquedaZapatillas.html", {"zapatillas":zapatillas})
+    else:
+        return render(request, "AppTienda/busquedaZapatillas.html", {"mensaje":"Ingrese nuevamente marca"})
+
+def buscarRemera(request):
+    if request.GET["marca"]:
+        marca = request.GET["marca"]
+        remeras=Remera.objects.filter(marca=marca)
+        
+        return render(request, "AppTienda/resultadosBusquedaRemeras.html", {"remeras":remeras})
+    else:
+        return render(request, "AppTienda/busquedaRemeras.html", {"mensaje":"Ingrese nuevamente marca"})
+
+def buscarPantalon(request):
+    if request.GET["marca"]:
+        marca = request.GET["marca"]
+        pantalones=Pantalon.objects.filter(marca=marca)
+        
+        return render(request, "AppTienda/resultadosBusquedaPantalones.html", {"pantalones":pantalones})
+    else:
+        return render(request, "AppTienda/busquedaPantalones.html", {"mensaje":"Ingrese nuevamente marca"})
